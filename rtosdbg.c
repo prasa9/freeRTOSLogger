fea3e2609783d55cb18b74e567fd23a4d098909d
prasa9 <106644425+prasa9@users.noreply.github.com>
2025-07-09 19:06:39 +0530

    Create rtosdbg.c
    Create stubs and helpers for basic skeleton

---
+++ rtosdbg.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/*******************************************************************************
+  Function    : StartTracingTasks()
+  Description :
+*******************************************************************************/
+static BOOL StartTracingTasks()(BOOL FirstCallInstance, const char* Cmd)
+  {
+  FirstCallInstance = FirstCallInstance;
+  Cmd   = Cmd;
+  
+  StartTaskTracing();
+
+  return TRUE;
+  }
+
+/*******************************************************************************
+  Function    : StartTracingTasks()
+  Description : dump traces of Task
+*******************************************************************************/
+static BOOL DumpTrace(BOOL FirstCallInstance, const char* Cmd)
+  {
+  BOOL MoreDataToArrive;
+  char Buf[256];
+  
+  Command = Command;
+  
+  MoreDataToArrive = RtosGetTraceInfo(FirstCallInstance, Buf, sizeof(Buf));
+  printf(Buf);
+
+  return MoreDataToArrive ;
+  }
+
+/*************************************************************************************
+  Function    : InstallDebugAction()
+  Description : helper function to add debug  traces to list say to display in HMI 
+*************************************************************************************/
+void InstallDebugAction(void)
+  {
+  
+  }
